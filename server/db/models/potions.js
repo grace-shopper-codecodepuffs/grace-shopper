d8c68901d98d630cbda80407512e7473386767b6
Harmony Richman <hmrichman@gmail.com>
2019-11-05 10:59:35 -0500

    updated user specs, seed file, to include first and last name of users

--- server/db/models/potions.js
+++ server/db/models/potions.js
@@ -2,28 +2,29 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Potion = db.define('potion', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    },
-    description: {
-        type: Sequelize.TEXT,
-        allowNull: true
-    },
-    quantity: {
-        type: Sequelize.INTEGER,
-        validate: {
-            min: 0
-        }
-    },
-    picture: {
-        type: Sequelize.STRING,
-        default: "https://vignette.wikia.nocookie.net/tlos-huggers/images/f/fe/Images.jpeg/revision/latest?cb=20190325152050",
-        allowNull: false
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
     }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0
+    }
+  },
+  picture: {
+    type: Sequelize.STRING,
+    default:
+      'https://vignette.wikia.nocookie.net/tlos-huggers/images/f/fe/Images.jpeg/revision/latest?cb=20190325152050',
+    allowNull: false
+  }
 })
 
 module.exports = Potion
@@ -32,13 +33,13 @@ module.exports = Potion
  * instanceMethods
  */
 Potion.prototype.isInStock = function() {
-    return this.quantity > 0
+  return this.quantity > 0
 }
 
 Potion.prototype.buySome = function(qty) {
-    if (this.quantity >= qty) {
-        this.quantity = this.quantity - qty
-    } else {
-        return "Not enough of requested potion in stock. Please check back later!"
-    }
+  if (this.quantity >= qty) {
+    this.quantity = this.quantity - qty
+  } else {
+    return 'Not enough of requested potion in stock. Please check back later!'
+  }
 }
