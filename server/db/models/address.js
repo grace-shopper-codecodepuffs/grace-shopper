f9a17af6ab89fec686f911b80494f10b18b8dc52
devAM100 <devismejia@gmail.com>
2019-11-05 13:03:49 -0500

    adding validations to required address inputs

--- server/db/models/address.js
+++ server/db/models/address.js
@@ -3,20 +3,40 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Address = db.define('address', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   line_1: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   },
   line_2: {
     type: Sequelize.STRING
   },
   city: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   },
   state: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   },
   zip: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmpty: false
+    }
   }
 })
 
