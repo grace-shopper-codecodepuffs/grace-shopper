fca092c66b4e4a0830f60b19a9cd4b89bdc209b5
Greta Gadbois <ggadbois@hmc.edu>
2019-11-05 12:46:48 -0500

    created order model, order associations, + one edit to potion model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,15 @@
 const User = require('./user')
 const Potion = require('./potions')
+const Order = require('./order')
+
+Order.belongsTo(User)
+User.hasMany(Order)
+
+Order.belongsToMany(Potion)
+Potion.hasMany(Order)
+
+Order.belongsTo(Address, {as: shippingAddress})
+Order.belongsTo(Address, {as: billingAddress})
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -16,5 +26,6 @@ const Potion = require('./potions')
  */
 module.exports = {
   User,
-  Potion
+  Potion,
+  Order
 }
