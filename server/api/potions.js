7683b92e7f75cb6f15adfcdb89819f891e735994
devAM100 <devismejia@gmail.com>
2019-11-06 16:49:51 -0500

    express route for single product done

--- server/api/potions.js
+++ server/api/potions.js
@@ -2,10 +2,20 @@ const router = require('express').Router()
 const {Potion} = require('../db/models')
 module.exports = router
 
+router.get('/:potionId', async (req, res, next) => {
+  try {
+    const potion = await Potion.findByPk(req.params.potionId)
+    if (!potion) {
+      res.status(404).send('This potion does not exist...yet!')
+    } else res.send(potion)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const potions = await Potion.findAll()
-    // console.log('potions', potions)
     if (!potions) {
       res.status(404).send('What happened to all the potions? :(')
     } else res.send(potions)
