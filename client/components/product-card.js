32d7259b319379135aa78c2adb85dde90597c211
devAM100 <devismejia@gmail.com>
2019-11-05 19:10:09 -0500

    saving progress for the night

--- client/components/product-card.js
+++ client/components/product-card.js
@@ -1,7 +1,44 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {connect} from 'react-redux'
+import axios from 'axios'
 
-class ProductCard extends React.Component {}
+class ProductCard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'Jello Legs',
+      description:
+        'This hex of a potion will hobble your enemies! Do you need to run away? Throw it at them. Do you want to prank your neighbor who lives at the top floor of a 4-story-walkup? They will hate you forever!',
+      image:
+        'https://storage.needpix.com/rsynced_images/potion-575711_1280.png',
+      price: '$49.99'
+    }
+  }
+  // async componentDidMount() {
+  //   try {
+  //     let {data} = await axios.get('/api/potions')
+  //     this.setState({data})
+  //   }
+  //   catch {
+  //     console.log('error')
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="product-card">
+        <img src={this.state.image} />
+        <h3 className="potion">{this.state.name}</h3>
+        <p className="description">{this.state.description}</p>
+        <h4 className="price">{this.state.price}</h4>
+        <input type="number" name="product-quantity" placeholder=" Quantity" />
+        <br />
+        <button>Add To Cart</button>
+      </div>
+    )
+  }
+}
 
 export default ProductCard
