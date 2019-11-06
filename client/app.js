1a0eb93c3f754232fdbd264c30604c8928644e67
devAM100 <devismejia@gmail.com>
2019-11-06 10:48:51 -0500

    finished product card

--- client/app.js
+++ client/app.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
-// import ProductCard from './components/product-card'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
-      {/* <ProductCard /> */}
     </div>
   )
 }
