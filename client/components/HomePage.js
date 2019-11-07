f8a653b53633a95d5294e90bd1260aa35c0c7a5d
Harmony Richman <hmrichman@gmail.com>
2019-11-07 11:25:22 -0500

    formatted the home page to center

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -5,11 +5,20 @@ import {Potions} from '../components/all-product-view'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Pattie's Potions Mwahahaha</h1>
-      <Link to="/potions" key="potions">
-        <img src="https://media.giphy.com/media/SY8xqBKwUiVtgrnQwJ/giphy.gif" />
-      </Link>
+    <div className="homePage">
+      <div className="fullPage">
+        <h1>Pattie's Potions Mwahahaha</h1>
+        <Link to="/potions" key="potions">
+          <img src="https://media.giphy.com/media/SY8xqBKwUiVtgrnQwJ/giphy.gif" />
+        </Link>
+        <p>
+          <h3>
+            <Link to="/potions" key="potions">
+              Enter at Your Own Risk!
+            </Link>
+          </h3>
+        </p>
+      </div>
     </div>
   )
 }
