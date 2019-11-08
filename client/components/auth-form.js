480619b3e80ca3f5a5b8082e1837ba76dfebaec6
Harmony Richman <hmrichman@gmail.com>
2019-11-08 16:08:40 -0500

    finished login and oath

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth, createdUser} from '../store'
+import {auth} from '../store'
 
 /**
  * COMPONENT
