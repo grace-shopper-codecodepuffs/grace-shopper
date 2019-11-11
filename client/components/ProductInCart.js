353f349d14ecf8f96ddb4dc8c589f231cabd1f3c
Agnieszka Dwulit <aga.i.dwulit@gmail.com>
2019-11-11 17:06:15 -0500

    post-changes commit, please approve

--- client/components/ProductInCart.js
+++ client/components/ProductInCart.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const ProductInCart = ({itemFromCart, potion}) => (
-  <div className="product-view">
+  <div className="cart-view">
     <div className="product-view-left">
-      <label className="product-image">{potion.picture}</label>
+      <label className="cart-image">{potion.picture}</label>
       <img src={potion.picture} />
       <h1>{potion.name}</h1>
       <p>{potion.description}</p>
