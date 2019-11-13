1af0f506a4da69f4a79c955840ac9bf586b4d916
devAM100 <devismejia@gmail.com>
2019-11-12 23:12:55 -0500

    homepage css for link at bottom of page

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -13,11 +13,11 @@ const HomePage = () => {
         <Link to="/potions" key="potions">
           <img src="https://media.giphy.com/media/SY8xqBKwUiVtgrnQwJ/giphy.gif" />
         </Link>
-        <h3>
+        <h2>
           <Link to="/potions" key="potions">
             Enter at Your Own Risk!
           </Link>
-        </h3>
+        </h2>
       </div>
     </div>
   )
